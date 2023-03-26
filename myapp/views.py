8796573744571956d5c00343b0dfeb6e2f7ed4b4
Kousthubh Yadavalli <98303359+Kousthubh02@users.noreply.github.com>
2023-03-26 07:20:45 +0530

    updated views.py

--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-import seaborn as sns
-import seaborn.apionly as sns_api
-import django_seaborn as sns_django
-import pandas as pd
+
 
 def home(request):
     return render(request,"index.html")
