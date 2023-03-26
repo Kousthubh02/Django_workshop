23a537355d74a863c3eed1e032cd3b84e451e87f
Kousthubh Yadavalli <98303359+Kousthubh02@users.noreply.github.com>
2023-03-26 06:41:10 +0530

    updated views.py

--- myapp/views.py
+++ myapp/views.py
@@ -6,11 +6,5 @@ import seaborn.apionly as sns_api
 import django_seaborn as sns_django
 import pandas as pd
 
-def plot_view(request):
-    data = pd.read_csv('path/to/data.csv')
-    plot = sns.scatterplot(x='x_col', y='y_col', data=data)
-    plot = sns_api.fig_to_dict(plot.figure)
-    return render(request, 'plot.html', {'plot': plot})
-
 def home(request):
     return render(request,"index.html")
